8463370c72acf0c17fc32a09b3c2f61b77fc5817
Egorq <64477831+Egor767@users.noreply.github.com>
2023-02-14 10:55:58 +0600

    Add files via upload
    ФИНАЛ

--- Shopp.cs
+++ Shopp.cs
@@ -12,9 +12,10 @@ namespace ConsoleApplication6
         public string dataizgot;
         public string srokgodn;
         public string proizvoditel;
-        public string dannyeoprodaje;
+        public string[] dannyeoprodaje1 = new string[3];
+        public int[] dannyeoprodaje2 = new int[3];
         public string kolvo;
-        public Good(string n, string da, string s, string p, string d, string c) { name = n; dataizgot = da; srokgodn = s; proizvoditel = p; dannyeoprodaje = d; kolvo = c; }
+        public Good(string n, string da, string s, string p, string[] d1, int[] d2, string c) { name = n; dataizgot = da; srokgodn = s; proizvoditel = p; dannyeoprodaje1 = d1; dannyeoprodaje2 = d2; kolvo = c; }
 
         public void Print()
         {
@@ -22,7 +23,7 @@ namespace ConsoleApplication6
             Console.WriteLine(dataizgot);
             Console.WriteLine(srokgodn);
             Console.WriteLine(proizvoditel);
-            Console.WriteLine(dannyeoprodaje);
+            Console.WriteLine("{0} - {1}\n{2} - {3}\n{4} - {5}", dannyeoprodaje1[0], dannyeoprodaje2[0], dannyeoprodaje1[1], dannyeoprodaje2[1], dannyeoprodaje1[2], dannyeoprodaje2[2]);
             Console.WriteLine(kolvo);
             Console.WriteLine();
         }
@@ -31,19 +32,6 @@ namespace ConsoleApplication6
 
     class Program
     {
-        public static void FullSale(ref List<Good> list)
-        {
-            foreach (Good i in list)
-            {
-                string b = i.dannyeoprodaje;
-                string c = i.kolvo;
-            }
-        }
-
-        public void Construktor()
-        {
-
-        }
 
         static void Main(string[] args)
         {
@@ -52,14 +40,28 @@ namespace ConsoleApplication6
             List<object> Namelist = new List<object>();
             string h = "1234567891";
             
-            //FullSale(ref Namelist);
+            //FullSale(ref Namelist); 
+            string[] e1 = new string[3]{"20.12.2022","21.12.2022","22.12.2022"};
+            int[] e2 = new int[3]{60,60,67};
+            Good Milk = new Good("Молоко", "20.12.2022", "20", "OOO Milk",e1, e2, "350");
             
-            Good Milk = new Good("Молоко", "20.12.2022", "20", "OOO Milk", "20.12.2022 - 60\n21.12.2022 - 60\n22.12.2022 - 67", "350");
-            Good Bread = new Good("Хлеб", "12.12.2022", "10", "Hrust", "12.12.2011 - 40\n13.12.2011 - 45\n14.12.2022 - 50", "200");
-            Good Coffe = new Good("Кофе", "18.12.2022", "90", "Nescaffe", "19.12.2022 - 10\n20.12.2022 - 30\n21.12.2022 - 20", "60");
-            Good Eggs = new Good("Яйца", "15.12.2022", "30", "Zavod", "16.12.2022 - 60\n17.12.2022 - 50\n14.18.2022 - 40", "150");
-            Good Chiken = new Good("Курица", "15.12.2022", "40", "Troekurovo", "16.12.2022 - 50\n17.12.2022 - 50\n18.12.2022 - 60", "200");                Namelist.Add(Milk);
-                
+            string[] e3 = new string[3]{"12.12.2011","13.12.2011","14.12.2022"};
+            int[] e4 =new int[3]{40,45,50};
+            Good Bread = new Good("Хлеб", "12.12.2022", "10", "Hrust",e3, e4, "200");
+            
+            string[] e5 = new string[3]{"19.12.2022", "20.12.2022","21.12.2022"};
+            int[] e6 = new int[3]{10, 30, 20};
+            Good Coffe = new Good("Кофе", "18.12.2022", "90", "Nescaffe", e5, e6,"60");
+            
+            string[] e7 = new string[3]{"16.12.2022", "17.12.2022", "14.18.2022"};
+            int[] e8 = new int[3]{60, 50, 40};
+            Good Eggs = new Good("Яйца", "15.12.2022", "30", "Zavod",e7,e8, "150");
+            
+            string[] e9 = new string[3]{"16.12.2022", "17.12.2022", "18.18.2022"};
+            int[] e11 = new int[3]{50, 50, 60};
+            Good Chiken = new Good("Курица", "15.12.2022", "40", "Troekurovo", e9,e11,"200"); 
+            
+            Namelist.Add(Milk);
             Namelist.Add(Bread);
             Namelist.Add(Coffe);
             Namelist.Add(Eggs);
@@ -108,15 +110,20 @@ namespace ConsoleApplication6
                         Console.WriteLine("Введите производителя");
                         d = Console.ReadLine();
                         Console.WriteLine("Введите данные о продаже");
-                        string e1 = Console.ReadLine();
-                        string e2 = Console.ReadLine();
-                        string e3 = Console.ReadLine();
-
-                        e=e1+"\n"+e2+"\n"+e3;
+                        for (int l = 0; l < 3; l++)
+                        {
+                            Console.WriteLine("Введите дату");
+                            e1[l] = Console.ReadLine();
+                            Console.WriteLine("Введите количество проданных");
+                            e2[l] = int.Parse(Console.ReadLine());
+                            
+                        }
+                        
+                        
                         Console.WriteLine("Введите данные о количестве на складе");
                         f=Console.ReadLine();
                         Console.WriteLine();
-                        Good meat = new Good(a, b, c, d, e, f);
+                        Good meat = new Good(a, b, c, d, e1,e2, f);
                         meat.Print();
                         Namelist.Add(meat);
                         Console.WriteLine("1 - Еще товар\n2 - Меню");
@@ -173,19 +180,17 @@ namespace ConsoleApplication6
                 }
                 else if (v == 4)
                 {
+                    int s=0;
                     Console.WriteLine("Полностью проданный товар:");
                     foreach (Good k in Namelist)
                     {
                         string sel=k.kolvo;
                         int sell=int.Parse(sel);
                         int sum=0;
-                        string s=k.dannyeoprodaje;
-                        string p="";
-                        for (int q=13;q<47;q+=16)
+                        for (int m=0;m<3;m++)
                         {
-                            p=p+s[q]+s[q+1];
-                            sum+=int.Parse(p);
-                            p="";
+                            s=k.dannyeoprodaje2[m];
+                            sum+=s;
                         }
                         if (sell==sum)
                         {
@@ -211,19 +216,17 @@ namespace ConsoleApplication6
                 }
                 else if (v==6)
                 {
+                    int m=0;
                     Console.WriteLine("Остаток товара\n");
                     foreach (Good k in Namelist)
                     {
                         string sel=k.kolvo;
                         int sell=int.Parse(sel);
                         int sum=0;
-                        string s=k.dannyeoprodaje;
-                        string p="";
-                        for (int q=13;q<47;q+=16)
+                        for (m=0;m<3;m++)
                         {
-                            p=p+s[q]+s[q+1];
-                            sum+=int.Parse(p);
-                            p="";
+                            int s=k.dannyeoprodaje2[m];
+                            sum+=s;
                         }
                         Console.WriteLine("{0} - {1} шт",k.name,sell-sum);
                     }
@@ -257,3 +260,21 @@ namespace ConsoleApplication6
 меню, где будут перечислены функции
 предусмотреть, шобы выборку товаров мы не производили пока не заполнили даннные
 можна предварительно просить кол-во товара
+string h=Console.ReadLine();
+                    string h1=h[0]+h[1];
+                    string h2=h[3]+h[4];
+                    string h3=h[6]+h[7]+h[8]+h[9];
+                    //01.34.6789
+                    foreach (Good y in Namelist)
+                    {
+                        int srok=int.Parse(y.srokgodn);
+                        string u=y.dataizgot;
+                        string u1=u[0]+u[1];
+                        string u2=u[3]+u[4];
+                        string u3=u[6]+u[7]+u[8]+u[9];
+                        if ((int.Parse(h1)<int.Parse(u1)) & (int.Parse(h2)<int.Parse(u2)) & (int.Parse(h3)<int.Parse(u3)))
+                        {
+                            Console.WriteLine(y.name);
+                        }
+                    }*/
+
